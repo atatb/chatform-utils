924280c72eab45b521191976b996e0136624389b
Kenji Nomura <kenji.nomura.0203@gmail.com>
2021-12-01 14:43:19 +0900

    Update gemspec and formats

--- lib/chatform/utils.rb
+++ lib/chatform/utils.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-require_relative "utils/version"
+require_relative 'utils/version'
 
 module Chatform
   module Utils
--- lib/chatform/utils/version.rb
+++ lib/chatform/utils/version.rb
@@ -2,6 +2,6 @@
 
 module Chatform
   module Utils
-    VERSION = "0.1.0"
+    VERSION = '0.1.0'
   end
 end
