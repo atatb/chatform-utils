365b84dd18f32fa260cf828706f52408264c5519
Kenji Nomura <kenji.nomura.0203@gmail.com>
2023-09-27 18:20:17 +0900

    Add parent params to convert func

--- lib/chatform/utils/hash_util.rb
+++ lib/chatform/utils/hash_util.rb
@@ -8,22 +8,23 @@ module Chatform
         #   key_func: ->(k) { k.to_s }
         #   value_func: ->(v, _k = nil) { v.to_s }
         #   object_func: ->(obj, keys_ = []) {}
-        def convert(object, keys = [], key_func: nil, value_func: nil, object_func: nil) # {{{
+        def convert(object, keys = [], parent: nil, key_func: nil, value_func: nil, object_func: nil) # {{{
           return if object_func && object_func.call(object, keys) == false
           if object.is_a?(Hash)
             object.each_with_object({}) do |(k, v), obj|
               key = key_func ? key_func.call(k) : k
-              value = convert(v, keys + [k], key_func: key_func, value_func: value_func, object_func: object_func)
+              value = convert(v, keys + [k], parent: object, key_func: key_func, value_func: value_func, object_func: object_func)
               next if value.nil?
               obj[key] = value
             end
           elsif object.is_a?(Array)
             object.each_with_object([]) do |v, obj|
-              value = convert(v, keys, key_func: key_func, value_func: value_func, object_func: object_func)
+              value = convert(v, keys, parent: obj, key_func: key_func, value_func: value_func, object_func: object_func)
               next if value.nil?
               obj << value
             end
           else
+            # value_func ? value_func.call(object, keys.last, parent: parent) : object # removed parent as not used
             value_func ? value_func.call(object, keys.last) : object
           end
         end # }}}
